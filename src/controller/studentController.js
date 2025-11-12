283bf0009660b721cb3eba623db705bb32090ddf
margolin <yurymargolin@inbox.ru>
2025-11-12 19:11:29 +0200

    wedw

--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -56,7 +56,7 @@ export const findByName = (req, res) => {
 
 export const countByNames = (req, res) => {
     const names = req.query.names;
-    const list = Array.isArray(names) ? names : [names];
+    const list = Array.isArray(names) ? names : [names ?? ''];
     const count = repo.countByNames(list);
     res.json(count)
 }
